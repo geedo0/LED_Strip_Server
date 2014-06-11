c7e2634818449956358af06588912e4973edd243
Gerardo Ravago <gcr@geedo.net>
2014-06-10 23:17:38 -0400

    Adds simple command-line interface to program strip

--- stripServer/main.c
+++ stripServer/main.c
@@ -2,14 +2,30 @@
 #include <wiringSerial.h>
 #include "main.h"
 
-//Test sending command to color lights blue
-char sendBuffer[] = {0x04, 0x00, 0x00, 0x00, 0xff};
-
 int main() {
 	int fd;
+	int input;
+	unsigned long inColor;
 	
 	fd = serialOpen("/dev/ttyAMA0", 9600);
-	setSinglePixel(fd, 15, getColor(200, 0, 0));
+	while(1) {
+		printf("Select an option:\n1) Color strip one color\n2) Color single pixel\n");
+		scanf("%i", &input);
+		printf("Enter RGB value in hex: ");
+		scanf("%x", &inColor);
+		switch(input) {
+			case 1:
+				setStripColor(fd, inColor);
+				break;
+			case 2:
+				printf("Enter pixel number: ");
+				scanf("%i", &input);
+				setSinglePixel(fd, input, inColor);
+				break;
+			default:
+				break;
+		}
+	}
 	serialClose(fd);	
 	
 	return 0;
@@ -33,7 +49,7 @@ void setSinglePixel(int fd, int pixel, unsigned long color) {
 
 void setRgbBuffer(char* buffer, unsigned long color) {
 	int i;
-	for(i = 0; i < 3; i++) {
+	for(i = 2; i >= 0; i--) {
 		buffer[i] = color & 0xff;
 		color >>= 8;
 	}
@@ -43,7 +59,7 @@ void setStripColor(int fd, unsigned long color) {
 	char buffer[5];
 	buffer[0] = 4;
 	buffer[1] = 0x00;
-	setRgbBuffer(buffer, color);
+	setRgbBuffer(buffer + 2, color);
 	putBuffer(fd, buffer, 5);
 }
 
